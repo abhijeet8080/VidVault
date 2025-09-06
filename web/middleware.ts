e451836c3105d430378dabac7d97906fde0a33d6
Abhijeet Kadam <kadamabhi1881@gmail.com>
2025-09-06 19:24:24 +0530

    Added a protected route

--- web/middleware.ts
+++ web/middleware.ts
@@ -4,7 +4,8 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const protectedRoutes = createRouteMatcher([
   "/dashboard",
   "/shared-link/p/:token",
-  '/videos'
+  '/videos',
+  '/library'
   
 ]);
 
